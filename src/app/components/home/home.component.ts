abd47a769c8353e494da5cc12be80f8c6253410a
fabiolajsm <suarezfabiola17@gmail.com>
2024-05-04 20:11:56 -0300

    Sprint 2: Estilos home solo web y sin boton del logout

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,23 +1,62 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../auth.service';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { CommonModule } from '@angular/common';
 
+export interface GamesList {
+  number: string;
+  title: string;
+  description: string;
+  img: string;
+}
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink, MatGridListModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
   authService = inject(AuthService);
   router = inject(Router);
+  username: string = '';
+  title: string = '¿Qué te gustaría jugar hoy?';
+  gamesList: GamesList[] = [
+    {
+      number: '01',
+      title: 'El Ahorcado',
+      description: 'Una descripcion',
+      img: '../../../assets/HangInThere.jpeg',
+    },
+    {
+      number: '02',
+      title: 'Mayor o Menor',
+      description: 'Una descripcion',
+      img: '../../../assets/HangInThere.jpeg',
+    },
+    {
+      number: '03',
+      title: 'Preguntados',
+      description: 'Una descripcion',
+      img: '../../../assets/HangInThere.jpeg',
+    },
+    {
+      number: '04',
+      title: 'Elementos',
+      description: 'Una descripcion',
+      img: '../../../assets/HangInThere.jpeg',
+    },
+  ];
 
   ngOnInit(): void {
     // TODO: buscar una mejor forma de manejar esto
     this.authService.user$.subscribe((user) => {
       if (!user) {
         this.router.navigateByUrl('/onboarding');
+      } else {
+        this.username = user.displayName?.toLocaleLowerCase() ?? '';
+        this.title = `¿Qué te gustaría jugar @${this.username}?`;
       }
     });
   }
